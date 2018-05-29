c3bd6f2855b3ddedc2418c9ed1d8e50c5e23c338
niteshyadav1671995 <ny1671995@gmail.com>
2018-05-29 14:04:40 +0530

    message

--- carelessly-lazy/carelessly-lazy.js
+++ carelessly-lazy/carelessly-lazy.js
@@ -1,12 +1,9 @@
 function lazyGuy() {
     this.hey = function(message){
       var reply="";
-      console.log(message);
       var exclamation = message.match(/!/g);
       var uppercase = message.match(/[A-Z]/g);
-      console.log(exclamation);
-      console.log(uppercase);
-      if(message.endsWith("?")){
+     if(message.endsWith("?")){
         reply="Sure.";
         return reply;
       }
